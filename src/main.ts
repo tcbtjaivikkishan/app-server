d82ce9fce6ccf3632d4d81160d813fe75b7647c1
Shahi Sharma <shahisharma270@example.com>
2026-06-02 12:30:58 +0530

    auto deploy test

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './instrumentation';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -35,7 +34,7 @@ async function bootstrap() {
     credentials: true,
   });
 
-  // 📄 Swagger — only available in development
+  // 📄 Swagger — only available in non-production
   if (process.env.NODE_ENV !== 'production') {
     const config = new DocumentBuilder()
       .setTitle('TCBT API')
@@ -54,62 +53,8 @@ async function bootstrap() {
   // ✅ Normal JSON parser for all other routes
   app.use(bodyParser.json());
 
-  await app.listen(3000);
-}
-
-bootstrap();
-=======
-import './instrumentation';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import helmet from 'helmet';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as bodyParser from 'body-parser';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    bodyParser: false, // ✅ Disable default parser (IMPORTANT)
-  });
-
-  // 🔐 Security
-  app.use(helmet());
-
-  // ✅ Validation
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
-
-  // 🌍 CORS
-  app.enableCors({
-    origin: '*', // restrict later
-    credentials: true,
-  });
-
-  const config = new DocumentBuilder()
-    .setTitle('My API')
-    .setDescription('Interactive API documentation')
-    .setVersion('1.0')
-    .addBearerAuth() // if using JWT
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-
-  SwaggerModule.setup('api-docs', app, document);
-
-  // 🔥 CRITICAL: RAW BODY for Zoho webhook ONLY
-  app.use('/payments/webhook', bodyParser.raw({ type: '*/*' }));
-
-  // ✅ Normal JSON parser for all other routes
-  app.use(bodyParser.json());
-
   const port = Number(process.env.PORT) || 3000;
   await app.listen(port);
 }
 
 bootstrap();
->>>>>>> 9fc6b85 (feat: add PM2 and OpenTelemetry setup)
